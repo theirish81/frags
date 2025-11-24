7a82fb9a91a9034fc8d3e2bfc7332fa95b4d44f8
Simone Pezzano <theirish81@gmail.com>
2025-11-24 10:04:51 +0100

    Cli progress (#8)
    * version alignment
    
    * engine selection

--- cli/main.go
+++ cli/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/spf13/viper"
 )
+
+const engineGemini = "gemini"
+const engineOllama = "ollama"
 
 type Config struct {
 	GeminiServiceAccountPath string `mapstructure:"GEMINI_SERVICE_ACCOUNT_PATH"`
 	GeminiProjectID          string `mapstructure:"GEMINI_PROJECT_ID"`
 	GeminiLocation           string `mapstructure:"GEMINI_LOCATION"`
 	ParallelWorkers          int    `mapstructure:"PARALLEL_WORKERS"`
+	OllamaBaseURL            string `mapstructure:"OLLAMA_BASE_URL"`
+	OllamaModel              string `mapstructure:"OLLAMA_MODEL"`
+	AiEngine                 string `mapstructure:"AI_ENGINE"`
+}
+
+func (c Config) guessAi() string {
+	switch strings.ToLower(c.AiEngine) {
+	case engineOllama:
+		return engineOllama
+	case engineGemini:
+		return engineGemini
+	}
+	if c.OllamaBaseURL != "" && c.OllamaModel != "" {
+		return engineOllama
+	}
+	if c.GeminiServiceAccountPath != "" && c.GeminiProjectID != "" && c.GeminiLocation != "" {
+		return engineGemini
+	}
+	return ""
 }
 
 var cfg = Config{}
