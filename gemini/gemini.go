e99996886df5ce2f3cd03b819eb83850423623b4
Simone Pezzano <theirish81@gmail.com>
2025-12-06 08:39:04 +0100

    Tools llm (#12)
    * gemini support for tools
    
    * tools support for ollama

--- gemini/gemini.go
+++ gemini/gemini.go
@@ -13,7 +13,8 @@ var temperature float32 = 0.1
 var topK float32 = 40
 var topP float32 = 0.9
 
-const contentType = "application/json"
+const jsonContentType = "application/json"
+const textContentType = "text/plain"
 const defaultModel = "gemini-2.5-flash"
 
 // Ai is a wrapper around the genai client for Frags
@@ -21,6 +22,7 @@ type Ai struct {
 	client    *genai.Client
 	content   []*genai.Content
 	modelName string
+	Functions frags.Functions
 }
 
 // NewAI creates a new Ai wrapper
@@ -29,6 +31,7 @@ func NewAI(client *genai.Client) *Ai {
 		client:    client,
 		content:   make([]*genai.Content, 0),
 		modelName: defaultModel,
+		Functions: frags.Functions{},
 	}
 }
 
@@ -38,37 +41,108 @@ func (d *Ai) New() frags.Ai {
 		client:    d.client,
 		content:   make([]*genai.Content, 0),
 		modelName: d.modelName,
+		Functions: d.Functions,
 	}
 }
 
 // Ask performs a query against the Gemini API, according to the Frags interface
-func (d *Ai) Ask(ctx context.Context, text string, schema frags.Schema, resources ...frags.ResourceData) ([]byte, error) {
+func (d *Ai) Ask(ctx context.Context, text string, schema *frags.Schema, tools frags.Tools, resources ...frags.ResourceData) ([]byte, error) {
 	parts := make([]*genai.Part, 0)
 	for _, resource := range resources {
 		parts = append(parts, genai.NewPartFromBytes(resource.Data, resource.MediaType))
 	}
 	parts = append(parts, genai.NewPartFromText(text))
-	genAiSchema := genai.Schema{}
-	if err := copier.Copy(&genAiSchema, &schema); err != nil {
-		return nil, err
+	genAiSchema := &genai.Schema{}
+	ct := jsonContentType
+	if schema == nil {
+		ct = textContentType
+		genAiSchema = nil
+	} else {
+		if err := copier.Copy(genAiSchema, schema); err != nil {
+			return nil, err
+		}
 	}
 	newMsg := genai.NewContentFromParts(parts, genai.RoleUser)
 
-	cfg := genai.GenerateContentConfig{
-		ResponseMIMEType: contentType,
-		ResponseSchema:   &genAiSchema,
-		Temperature:      &temperature,
-		TopK:             &topK,
-		TopP:             &topP,
-	}
-	d.content = append(d.content, newMsg)
-	res, err := d.client.Models.GenerateContent(ctx, d.modelName, d.content, &cfg)
+	tx, err := d.configureTools(tools)
 	if err != nil {
 		return nil, err
 	}
-	d.content = append(d.content, res.Candidates[0].Content)
-	out := joinParts(res.Candidates[0].Content.Parts)
+	cfg := genai.GenerateContentConfig{
+		ResponseMIMEType: ct,
+		ResponseSchema:   genAiSchema,
+		Temperature:      &temperature,
+		TopK:             &topK,
+		TopP:             &topP,
+		Tools:            tx,
+	}
+	keepGoing := true
+	out := ""
+	d.content = append(d.content, newMsg)
+	for keepGoing {
+		res, err := d.client.Models.GenerateContent(ctx, d.modelName, d.content, &cfg)
+		if err != nil {
+			return nil, err
+		}
+		d.content = append(d.content, res.Candidates[0].Content)
+		if res.FunctionCalls() != nil {
+			for _, fc := range res.FunctionCalls() {
+				d.content = append(d.content, genai.NewContentFromFunctionCall(fc.Name, fc.Args, genai.RoleModel))
+				fres, ferr := d.Functions[fc.Name].Func(fc.Args)
+				if ferr != nil {
+					return nil, ferr
+				} else {
+					d.content = append(d.content, genai.NewContentFromFunctionResponse(fc.Name, fres, genai.RoleUser))
+				}
+			}
+			keepGoing = true
+		} else {
+			keepGoing = false
+			d.content = append(d.content, res.Candidates[0].Content)
+			out = joinParts(res.Candidates[0].Content.Parts)
+		}
+	}
 	return []byte(out), nil
+}
+
+func (d *Ai) configureTools(tools frags.Tools) ([]*genai.Tool, error) {
+	tx := make([]*genai.Tool, 0)
+	fd := make([]*genai.FunctionDeclaration, 0)
+	for _, tool := range tools {
+		if tool.Type == frags.ToolTypeFunction {
+			if fx, found := d.Functions[tool.Name]; found {
+				pSchema := fx.Schema
+				if tool.Parameters != nil {
+					pSchema = tool.Parameters
+				}
+				genAiPSchema := &genai.Schema{}
+				if err := copier.Copy(genAiPSchema, pSchema); err != nil {
+					return nil, err
+				}
+				description := fx.Description
+				if len(tool.Description) > 0 {
+					description = tool.Description
+				}
+				fd = append(fd, &genai.FunctionDeclaration{
+					Name:        tool.Name,
+					Description: description,
+					Parameters:  genAiPSchema,
+				})
+			}
+		}
+	}
+	if len(fd) > 0 {
+		tx = append(tx, &genai.Tool{
+			FunctionDeclarations: fd,
+		})
+	}
+
+	if tools.HasType(frags.ToolTypeInternetSearch) {
+		tx = append(tx, &genai.Tool{
+			GoogleSearch: &genai.GoogleSearch{},
+		})
+	}
+	return tx, nil
 }
 
 func joinParts(parts []*genai.Part) string {
--- ollama/dto.go
+++ ollama/dto.go
@@ -4,19 +4,55 @@ import "github.com/theirish81/frags"
 
 // Message represents a message sent to the LLM.
 type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
+	Role       string     `json:"role"`
+	Content    string     `json:"content"`
+	ToolCalls  []ToolCall `json:"tool_calls,omitempty"`
+	ToolCallID string     `json:"tool_call_id,omitempty"`
 }
 
 // Request represents a request to Ollama
 type Request struct {
-	Model    string       `json:"model"`
-	Messages []Message    `json:"messages"`
-	Format   frags.Schema `json:"format,omitempty"`
-	Stream   bool         `json:"stream"`
+	Model    string           `json:"model"`
+	Messages []Message        `json:"messages"`
+	Format   *frags.Schema    `json:"format,omitempty"`
+	Stream   bool             `json:"stream"`
+	Think    bool             `json:"think"`
+	Tools    []ToolDefinition `json:"tools,omitempty"`
+	Options  Options          `json:"options,omitempty"`
+}
+
+type ToolDefinition struct {
+	Type     string      `json:"type" yaml:"type"`
+	Function FunctionDef `json:"function" yaml:"function"`
+}
+
+// FunctionDef represents a function definition
+type FunctionDef struct {
+	Name        string       `json:"name" yaml:"name"`
+	Description string       `json:"description" yaml:"description"`
+	Parameters  frags.Schema `json:"parameters" yaml:"parameters"`
 }
 
 // Response represents a response from Ollama
 type Response struct {
 	Message Message `json:"message"`
+}
+
+type ToolCall struct {
+	ID       string       `json:"id,omitempty"`
+	Type     string       `json:"type"`
+	Function FunctionCall `json:"function"`
+}
+
+// FunctionCall represents a function call
+type FunctionCall struct {
+	Name      string         `json:"name"`
+	Arguments map[string]any `json:"arguments"`
+}
+
+type Options struct {
+	NumPredict        int      `json:"num_predict,omitempty"`
+	Stop              []string `json:"stop,omitempty"`
+	Temperature       float64  `json:"temperature,omitempty"`
+	RepetitionPenalty float64  `json:"repeat_penalty,omitempty"`
 }
