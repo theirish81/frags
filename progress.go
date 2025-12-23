a21b1463a0a23b02888a7b6fffd7c16dae17e21a
theirish81 <theirish81@gmail.com>
2025-12-23 07:50:21 +0100

    using enumerated constant, documentation

--- progress.go
+++ progress.go
@@ -1,12 +1,33 @@
+/*
+ * Copyright (C) 2025 Simone Pezzano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package frags
 
-const progressActionStart = "START"
-const progressActionEnd = "END"
-const progressActionError = "ERROR"
+type ProgressAction string
+
+const (
+	progressActionStart ProgressAction = "START"
+	progressActionEnd   ProgressAction = "END"
+	progressActionError ProgressAction = "ERROR"
+)
 
 // ProgressMessage is a message sent to the progress channel.
 type ProgressMessage struct {
-	Action    string
+	Action    ProgressAction
 	Session   string
 	Phase     int
 	Iteration int
@@ -14,7 +35,7 @@ type ProgressMessage struct {
 }
 
 // sendProgress sends a progress message to the progress channel
-func (r *Runner[T]) sendProgress(action string, sessionID string, phaseIndex int, iteration int, err error) {
+func (r *Runner[T]) sendProgress(action ProgressAction, sessionID string, phaseIndex int, iteration int, err error) {
 	if r.progressChannel != nil {
 		r.progressChannel <- ProgressMessage{
 			Action:    action,
